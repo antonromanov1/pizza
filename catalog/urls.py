a76b93bacb1701d90314e95eb1120f70aab5849d
antonromanov1 <antonbur12@gmail.com>
2017-04-10 00:21:31 +0300

    5th commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,9 @@ import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^pizza$', views.pizza, name='pizza'),
+    url(r'^hot$', views.pizza, name='hot'),
+    url(r'^salads$', views.pizza, name='salads'),
+    url(r'^drinks$', views.pizza, name='drinks'),
+    url(r'^desserts$', views.pizza, name='desserts')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "catalog/index.html", {})
+
+def pizza(request):
+    return render(request, "catalog/pizza.html", {})
+
+def hot(request):
+    return render(request, "catalog/hot.html", {})
+
+def salads(request):
+    return render(request, "catalog/salads.html", {})
+
+def drinks(request):
+    return render(request, "catalog/drinks.html", {})
+
+def desserts(request):
+    return render(request, "catalog/desserts.html", {})
